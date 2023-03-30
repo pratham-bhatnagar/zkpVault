0313a0ca99de55c963a2e49c7d6112ec3224c125
Pratham Bhatnagar <prathambhatnagar2803@gmai.com>
2023-03-31 03:38:28 +0530

    Update Cors

--- server/src/index.js
+++ server/src/index.js
@@ -1,22 +1,13 @@
 import express from "express";
 import cors from "cors";
-import { generateCallData ,generateProof} from "./lib/zkutils.js";
+import { generateCallDataCredit,generateProofCredit,generateCallDataTwitter,generateProofTwitter,generateCallDataAge,generateProofAge} from "./lib/zkutils.js";
 
 const app = express();
 const port = 8080;
-const corsOptions = {
-  // To allow requests from client
-  origin: [
-    "http://localhost:3000",
-    "http://localhost:3001",
-    "http://localhost:3002",
-  ],
-  credentials: true,
-  exposedHeaders: ["set-cookie"],
-};
+
 
 app.set("trust proxy", true);
-app.use(cors());
+
 app.get("/", (req, res) => {
   res.json({
     success: true,
@@ -24,6 +15,59 @@ app.get("/", (req, res) => {
     uptime: process.uptime(),
   });
 });
+
+// Twitter Verification
+app.get("/api/twitter/generate-call-data", async(req,res,next)=>{
+try{
+  console.log("Generating proof...");
+    const followers = req.query.followers;
+    const threshold = req.query.threshold;
+    // check if creditScore is a number
+    if (isNaN(followers) && isNaN(threshold)) {
+      return res.status(400).send("followers must be a number");
+    }
+    const { a, b, c, Input } = await generateCallDataTwitter(followers,threshold);
+
+    if (a === null || b === null || c === null || Input === null) {
+      return res.status(400).send("Error generating call data");
+    }
+
+    console.log("Call Data Generated");
+    console.log("a", a);
+    console.log("b", b);
+    console.log("c", c);
+    console.log("Input", Input);
+    return res.status(200).send({ a, b, c, Input });
+
+}catch(err){
+  console.log(`Error Message ${err.message}`)
+  next(err);
+}
+})
+
+app.get("/api/twitter/generate-proof", async (req, res, next) => {
+  try {
+    console.log("Generating proof...");
+    const followers = req.query.followers;
+    const threshold = req.query.threshold;
+    // check if creditScore is a number
+    if (isNaN(followers) && isNaN(threshold)) {
+      return res.status(400).send("Followes and Threshold must be a number");
+    }
+    const { proof, publicSignals } = await generateProofTwitter(followers,threshold);
+
+    // check if proofJson is null
+    if (proof == null) {
+      return res.status(400).send("creditScore must more than 15");
+    }
+    return res.status(200).json({ proof, publicSignals });
+  } catch (error) {
+    console.log(`Error Message ${error.message}`);
+    next(error);
+  }
+});
+
+
 
 
 // Credit Score
@@ -35,7 +79,7 @@ app.get("/api/credit/generate-call-data", async (req, res, next) => {
     if (isNaN(creditScore)) {
       return res.status(400).send("creditScore must be a number");
     }
-    const { a, b, c, Input } = await generateCallData(creditScore);
+    const { a, b, c, Input } = await generateCallDataCredit(creditScore);
 
     if (a === null || b === null || c === null || Input === null) {
       return res.status(400).send("Error generating call data");
@@ -61,7 +105,7 @@ app.get("/api/credit/generate-proof", async (req, res, next) => {
     if (isNaN(creditScore)) {
       return res.status(400).send("creditScore must be a number");
     }
-    const { proof, publicSignals } = await generateProof(creditScore);
+    const { proof, publicSignals } = await generateProofCredit(creditScore);
 
     // check if proofJson is null
     if (proof == null) {
@@ -73,6 +117,55 @@ app.get("/api/credit/generate-proof", async (req, res, next) => {
     next(error);
   }
 });
+
+// Age Verification
+app.get("/api/age/generate-call-data", async (req, res, next) => {
+  try {
+    console.log("Generating proof...");
+    const age = req.query.age;
+    // check if age is a number
+    if (isNaN(age)) {
+      return res.status(400).send("age must be a number");
+    }
+    const { a, b, c, Input } = await generateCallDataAge(age);
+
+    if (a === null || b === null || c === null || Input === null) {
+      return res.status(400).send("Error generating call data");
+    }
+
+    console.log("Call Data Generated");
+    console.log("a", a);
+    console.log("b", b);
+    console.log("c", c);
+    console.log("Input", Input);
+    return res.status(200).send({ a, b, c, Input });
+  } catch (error) {
+    console.log(`Error Message ${error.message}`);
+    next(error);
+  }
+});
+
+app.get("/api/age/generate-proof", async (req, res, next) => {
+  try {
+    console.log("Generating proof...");
+    const age = req.query.age;
+    // check if age is a number
+    if (isNaN(creditScore)) {
+      return res.status(400).send("creditScore must be a number");
+    }
+    const { proof, publicSignals } = await generateProofAge(age);
+
+    // check if proofJson is null
+    if (proof == null) {
+      return res.status(400).send("age must more than 18");
+    }
+    return res.status(200).json({ proof, publicSignals });
+  } catch (error) {
+    console.log(`Error Message ${error.message}`);
+    next(error);
+  }
+});
+
 
 app.use("*", (req, res) => {
   res.status(405).json({
--- server/src/lib/zkutils.js
+++ server/src/lib/zkutils.js
@@ -3,13 +3,16 @@ import { wrongProof, wrongPublicSignals } from "../public/creditScoreWrongProof.
 
 const CreditScoreWasmPath = './src/public/creditScoreCircuit.wasm'
 const CreditScoreFinalZkeyPath = './src/public/creditScoreCircuit_0001.zkey'
-const CreditScoreVerificationKeyPath = './src/public/creditScoreVerification_key.json'
+const TwitterWasmPath = '././src/public/TwitterFollower.wasm'
+const TwitterFinalZkeyPath = './src/public/Twittercircuit_0001.zkey'
+const AgeWasmPath ='./src/public/Age.wasm'
+const AgeFinalZkeyPath ='./src/public/age_0001.zkey'
 
 
-export async function generateCallData(creditScore) {
+export async function generateCallDataAge(age) {
     try {
         console.log("Generating Proofs...");
-        const { proof, publicSignals } = await generateProof(creditScore);
+        const { proof, publicSignals } = await generateProofAge(age);
         console.log(proof);
         console.log("Generating call data...");
         const calldata = await snarkjs.groth16.exportSolidityCallData(proof, publicSignals);
@@ -39,7 +42,63 @@ export async function generateCallData(creditScore) {
     }
 }
 
-export async function generateProof(creditScore) {
+export async function generateProofAge(age) {
+    try {
+        const inputSignal = {
+            "min_age": 18,
+            "age": age
+        };
+        const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+            inputSignal,
+            AgeWasmPath,
+            AgeFinalZkeyPath
+        );
+        return { proof, publicSignals };
+    }
+    catch (error) {
+        // check if it returns Error: Assert Failed.
+        if (error.message.includes("Assert Failed")) {
+            return { proof: wrongProof, publicSignals: wrongPublicSignals };
+        }
+    }
+}
+
+
+
+export async function generateCallDataCredit(creditScore) {
+    try {
+        console.log("Generating Proofs...");
+        const { proof, publicSignals } = await generateProofCredit(creditScore);
+        console.log(proof);
+        console.log("Generating call data...");
+        const calldata = await snarkjs.groth16.exportSolidityCallData(proof, publicSignals);
+        const argv = calldata
+            .replace(/["[\]\s]/g, "")
+            .split(",")
+            .map((x) => BigInt(x).toString());
+        console.log("ARGV",calldata)
+        const a = [argv[0], argv[1]];
+        const b = [
+            [argv[2], argv[3]],
+            [argv[4], argv[5]],
+        ];
+        const c = [argv[6], argv[7]];
+        const Input = [];
+
+        for (let i = 8; i < argv.length; i++) {
+            Input.push(argv[i]);
+        }
+
+        return { a, b, c, Input };
+    }
+    catch (error) {
+        console.log("Error during exportSolidityCallData");
+        console.log(`Error Message ${error.message}`);
+        return null;
+    }
+}
+
+export async function generateProofCredit(creditScore) {
     try {
         const inputSignal = {
             "threshold": 25,
@@ -60,3 +119,60 @@ export async function generateProof(creditScore) {
     }
 }
 
+
+// Twitter
+
+export async function generateCallDataTwitter(followers,threshold) {
+    try {
+        console.log("Generating Proofs...");
+        const { proof, publicSignals } = await generateProofTwitter(followers,threshold);
+        console.log(proof);
+        console.log("Generating call data...");
+        const calldata = await snarkjs.groth16.exportSolidityCallData(proof, publicSignals);
+        const argv = calldata
+            .replace(/["[\]\s]/g, "")
+            .split(",")
+            .map((x) => BigInt(x).toString());
+        console.log("ARGV",calldata)
+        const a = [argv[0], argv[1]];
+        const b = [
+            [argv[2], argv[3]],
+            [argv[4], argv[5]],
+        ];
+        const c = [argv[6], argv[7]];
+        const Input = [];
+
+        for (let i = 8; i < argv.length; i++) {
+            Input.push(argv[i]);
+        }
+
+        return { a, b, c, Input };
+    }
+    catch (error) {
+        console.log("Error during exportSolidityCallData");
+        console.log(`Error Message ${error.message}`);
+        return null;
+    }
+}
+
+export async function generateProofTwitter(followers,threshold) {
+    try {
+        const inputSignal = {
+            "threshold_followers": threshold,
+            "user_followers": followers
+            
+        };
+        const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+            inputSignal,
+            TwitterWasmPath,
+            TwitterFinalZkeyPath
+        );
+        return { proof, publicSignals };
+    }
+    catch (error) {
+        // check if it returns Error: Assert Failed.
+        if (error.message.includes("Assert Failed")) {
+            return { proof: wrongProof, publicSignals: wrongPublicSignals };
+        }
+    }
+}
